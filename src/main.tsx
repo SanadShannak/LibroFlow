e2bc9c70c9d7c2af949eea7d74737aa2946f79be
Sanad Shannak <sanadshannak.ss@gmail.com>
2025-04-11 22:22:00 +0300

    setup routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { MantineProvider } from '@mantine/core';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MantineProvider
+      theme={{}} // Optional: you can add your custom theme here
+      defaultColorScheme="light"
+    >
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
