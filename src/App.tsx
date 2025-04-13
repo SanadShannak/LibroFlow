81a725117f8c33a882eae870c28762976f473a68
MutasimAlatawneh <129550250+MutasimAlatawneh@users.noreply.github.com>
2025-04-14 00:07:18 +0300

    I just start doing dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
 import ForgotPassword from './pages/auth/ForgotPassword';
-
-
+import AdminDashboard from './pages/dashboard/AdminDashboard';  
 
 function App() {
   return (
@@ -12,6 +11,10 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
+
+      {/* Admin Dashboard Route */}
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
+
       {/* Fallback Route */}
       <Route path="*" element={<LoginPage />} />
     </Routes>
